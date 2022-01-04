0558913dab2ef207ea1766cd25cae9878b3b10c5
Sathira Fernando <wsdfdo@gmail.com>
2022-01-04 23:25:06 +0530

    formatted 2 files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import React,{Component} from 'react';
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
- import Sidebar from './components/sidebar/Sidebar';
-import About from './components/about/About';
-import Education from './components/Education/Education';
-import Interest from './components/skills/Interest'
-import CarouselImages from './components/carousel/CarouselImages.js'
-
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
+import About from "./components/about/About";
+import Education from "./components/Education/Education";
+import Interest from "./components/skills/Interest";
+// import CarouselImages from './components/carousel/CarouselImages.js'
 
 class App extends Component {
-
-  render(){
-  return (
-    <Router>
-      <div className="App">
+  render() {
+    return (
+      <Router>
+        <div className="App">
           <div className="side">
-            <nav className="navbar side navbar-expand-lg navbar-light p-0" >
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{zIndex:'1'}}>
+            <nav className="navbar side navbar-expand-lg navbar-light p-0">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                style={{ zIndex: "1" }}
+              >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
                 <Sidebar />
               </div>
-            </nav>  
+            </nav>
           </div>
-            <div className="main">
-              <div>
-
-                 <CarouselImages />
-              </div>
-                 <About />
-                <Interest />
-                <Education />
-            </div>
-      </div>
-   </Router>
-  );
+          <div className="main">
+            <div style={{ minHeight: "100px" }}>{/* <CarouselImages /> */}</div>
+            <About />
+            <Interest />
+            <Education />
+            <Education />
+            <Education />
+            <Education />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
