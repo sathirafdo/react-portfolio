2b3ae1bc31ca562cf8372ec8506a81a6fe8c17d8
Sathira Fernando <wsdfdo@gmail.com>
2022-01-06 11:33:58 +0530

    Updates

---
+++ precache-manifest.1e293682a0118f38ec88f85f6aea2914.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebcc7709179ec22063e7110e420f14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb1469d6babec9b1c92b",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "da44db80ba36ef4a588d",
+    "url": "/static/css/main.57faebcd.chunk.css"
+  },
+  {
+    "revision": "fb1469d6babec9b1c92b",
+    "url": "/static/js/2.617288e3.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.617288e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da44db80ba36ef4a588d",
+    "url": "/static/js/main.2bd859fe.chunk.js"
+  },
+  {
+    "revision": "79d439c7be0dd499b205",
+    "url": "/static/js/runtime-main.e9933245.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/static/media/laptop1.87561258.jpg"
+  },
+  {
+    "revision": "b414597e01e36cc52f34fe8b79ee3d09",
+    "url": "/static/media/logo1.b414597e.png"
+  },
+  {
+    "revision": "27a03886cfc6dcbf3e19b0d747c14ccb",
+    "url": "/static/media/me.27a03886.jpg"
+  }
+]);
